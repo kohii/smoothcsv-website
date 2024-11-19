c8cc796798f14ee5d1cfe552d9992e471d3f6fd3
kohii <kohii.tokyo@gmail.com>
2024-11-19 21:16:07 +0900

    reformat

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,24 @@
-import colors from "tailwindcss/colors";
 import typography from "@tailwindcss/typography";
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-    extend: {
-      textColor: {
+		extend: {
+			textColor: {
 				primary: "rgb(36, 36, 36)",
 				secondary: "rgb(71, 85, 105)",
-        link: "#007A6C",
+				link: "#007A6C",
 			},
-      borderColor: {
+			borderColor: {
 				DEFAULT: "rgb(229, 229, 229)",
 				focused: colors.sky[600],
 			},
-      colors: {
-        primary: "#008575"
-      }
-    },
+			colors: {
+				primary: "#008575",
+			},
+		},
 	},
-	plugins: [
-		typography,
-	],
-}
+	plugins: [typography],
+};
